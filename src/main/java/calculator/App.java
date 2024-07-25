7e312054fd4039295721c97a7e6a688e4b217158
mj-song00 <niveus128@gmail.com>
2024-07-25 22:06:52 +0900

    feat: 요구사항 1-2 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,5 +12,9 @@ public class App {
 
         System.out.println("첫 번째 숫자를 입력하세요: " + num1 );
         System.out.println("두 번째 숫자를 입력하세요: " + num2 );
+
+        char op = sc.next().charAt(0); // charAt(index): 입력 받은 string 타입을 char 타입 으로 변환
+        System.out.println("사친 연산 기호를 입력하세요: " + op );
+
     }
 }
