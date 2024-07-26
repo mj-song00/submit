7c1161e9eaeba88201deac6ec866ee3b9a1b0807
mj-song00 <niveus128@gmail.com>
2024-07-26 14:19:20 +0900

    feat: 요구사항4번째 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -7,40 +8,53 @@ public class App {
     public static void main(String[] args) {
        // level1 -1
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        char op = sc.next().charAt(0); // charAt(index): 입력 받은 string 타입을 char 타입 으로 변환
 
-        System.out.println("첫 번째 숫자를 입력하세요: " + num1 );
-        System.out.println("두 번째 숫자를 입력하세요: " + num2 );
+        while(true){
+            System.out.println("첫 번째 숫자를 입력하세요: " );
+            int num1 = sc.nextInt();
 
-        switch (op){
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-            case '+':
-                int sum = num1 + num2;
-                System.out.println("결과 : " + sum);
-                break;
-            case '-':
-                int subtract = num1 - num2;
-                System.out.println("결과 : " +subtract);
-                break;
-            case '*':
-                int multiply = num1 * num2;
-                System.out.println("결과 : " + multiply);
-                break;
-            case '/' :
+            System.out.println("연산자를 입력하세요: ");
+            char op = sc.next().charAt(0); // charAt(index): 입력 받은 string 타입을 char 타입 으로 변환
+
+            switch (op){
+                case '+':
+                    int sum = num1 + num2;
+                    System.out.println("결과 : " + sum);
+                    break;
+                case '-':
+                    int subtract = num1 - num2;
+                    System.out.println("결과 : " +subtract);
+                    break;
+                case '*':
+                    int multiply = num1 * num2;
+                    System.out.println("결과 : " + multiply);
+                    break;
+                case '/' :
 //                if (num2 != 0) {
 //                    int divide = num1 / num2;
 //                    System.out.println("결과 : " + divide);
 //                }else{
 //                    System.out.println("num2는 0이 될 수 없습니다.");
 //                }
-                // 32~ 37 번재 if문 삼항연산자로 변환
-                System.out.println((num2 != 0) ?"결과 : " + num1 / num2 : "num2는 0이 될 수 없습니다.");
+
+                    // 32~37줄  if문 삼항연산자로 변환
+                    System.out.println((num2 != 0) ?"결과 : " + num1 / num2 : "num2는 0이 될 수 없습니다.");
+                    break;
+                default:
+                    System.out.println("잘못된 입력값 입니다.");
+                    break;
+            }
+            sc.nextLine(); // 오늘 물어볼것
+
+            System.out.println("더 계산 하시겠습니까? (exit 입력시 종료) :");
+            String str = sc.nextLine();
+            if (Objects.equals(str, "exit")){
+                System.out.println("프로그램을 종료합니다.");
                 break;
-            default:
-                System.out.println("잘못된 입력값 입니다.");
-                break;
+            }
         }
 
 
