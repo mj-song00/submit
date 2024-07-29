9d38c2abbee72d949a92ab52a0827f551e9e64f6
mj-song00 <niveus128@gmail.com>
2024-07-29 21:30:56 +0900

    feat: leve2 2-1 요구사항 완료

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,35 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
+    ArrayList<Double> doubleList = new ArrayList<>();
+
+    public  double  calulate(int num1, int num2, char operator){
+        double result;
+
+        switch (operator){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0){
+                    throw new Error("두 번째 숫자는 0이 될수 없어용");
+                }else{
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                throw new Error( operator + "대신 알맞은 사칙연산 기호를 입력해 주세용");
+        }
+
+        return result;
+    }
 
 }
