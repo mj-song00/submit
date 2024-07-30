c602252108363ab3985292ca7195c0d3de7fafe9
mj-song00 <niveus128@gmail.com>
2024-07-30 17:03:33 +0900

    feat: level2 2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,43 +8,71 @@ public class App {
 
         Calculator ca = new Calculator();
 
+
         Scanner sc = new Scanner(System.in);
 
         while(true){
 
-            System.out.println("첫 번째 숫자를 입력하세요: " );
-            int num1 = sc.nextInt();
+            System.out.println("1. 계산하기 | 2. 원의 넓이 구하기");
+            int menu = sc.nextInt();
 
-            System.out.println("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            if(menu == 1){
+                System.out.println("첫 번째 숫자를 입력하세요: " );
+                int num1 = sc.nextInt();
 
-            System.out.println("연산자를 입력하세요: ");
-            char op = sc.next().charAt(0); // charAt(index): 입력 받은 string 타입을 char 타입 으로 변환
+                System.out.println("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt();
 
-            double result = ca.calulate(num1, num2, op);
+                System.out.println("연산자를 입력하세요: ");
+                char op = sc.next().charAt(0); // charAt(index): 입력 받은 string 타입을 char 타입 으로 변환
 
-           System.out.println("가장 먼저 저장된 연산결과를 삭제하시겠습니까?(remove 입력시 삭제)");
-            sc.nextLine();
-            String deleteKeyWord = sc.nextLine();
+                double result = ca.calulate(num1, num2, op);
 
-            if (Objects.equals(deleteKeyWord, "remove")){
-                ca.setRemoveDoubleList();
-                ca.setAddDoubleList();
-            }else{
-                ca.setAddDoubleList();
-            }
+                System.out.println("가장 먼저 저장된 연산결과를 삭제하시겠습니까?(remove 입력시 삭제)");
+                sc.nextLine();
+                String deleteKeyWord = sc.nextLine();
 
-            System.out.println("저장된 연산 결과를 조회하시겠습니까?(inquiry 입력시 조회)");
-            String checkKeyWord = sc.nextLine();
-            if (Objects.equals(checkKeyWord,"inquiry")){
-                System.out.println(ca.getDoubleList());
-            }
+                if (Objects.equals(deleteKeyWord, "remove")){
+                    ca.setRemoveDoubleList();
+                    ca.setAddDoubleList();
+                }else{
+                    ca.setAddDoubleList();
+                }
 
-            System.out.println("더 계산 하시겠습니까? (exit 입력시 종료) :");
-            String str = sc.nextLine();
-            if (Objects.equals(str, "exit")){
-                System.out.println("프로그램을 종료합니다.");
-                break;
+                System.out.println("저장된 연산 결과를 조회하시겠습니까?(inquiry 입력시 조회)");
+                String checkKeyWord = sc.nextLine();
+                if (Objects.equals(checkKeyWord,"inquiry")){
+                    System.out.println(ca.getDoubleList());
+                }
+
+                System.out.println("더 계산 하시겠습니까? (exit 입력시 종료) :");
+                String str = sc.nextLine();
+                if (Objects.equals(str, "exit")){
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
+            } else if (menu ==2){
+                //원의 넓이 구하기
+                System.out.println("반지름을 입력해주세요: " );
+                int r = sc.nextInt();
+
+                double result = ca.calculateCircleArea(r);
+                ca.setAddCircleList(r);
+                System.out.println("원의 넓이는 " + result + "입니다.");
+
+                System.out.println("저장된 결과를 조회하시겠습니까?(inquiry 입력시 조회)");
+                sc.nextLine();
+                String checkKeyWord = sc.nextLine();
+                if (Objects.equals(checkKeyWord,"inquiry")){
+                    System.out.println(ca.getCircleList());
+                }
+
+                System.out.println("더 계산 하시겠습니까? (exit 입력시 종료) :");
+                String str = sc.nextLine();
+                if (Objects.equals(str, "exit")){
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
             }
         }
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,7 +6,8 @@ public class Calculator {
   final  ArrayList<Double> doubleList = new ArrayList<>();
     Double lastResult;
 
-    double  calulate(int num1, int num2, char operator){
+    //간단한 계산기
+   public double  calulate(int num1, int num2, char operator){
         double result = 0;
 
         switch (operator){
@@ -31,7 +32,7 @@ public class Calculator {
         }
         lastResult = result;
         return result;
-    }
+   }
 
     public ArrayList<Double> getDoubleList(){
         return doubleList;
@@ -48,4 +49,34 @@ public class Calculator {
             throw new Error("인덱스가 존재하지 않습니다.");
         }
     }
+
+    // 원의 넓이 계산
+    final  ArrayList<Double> circleList = new ArrayList<>();
+    public  double calculateCircleArea(int r){
+//        double circle = 0;
+
+       double circle = getCircle(r) * circumference;
+
+        return circle;
+    }
+
+    // 원주율 3.14 고정
+    // static final -> 모든 영역에서 고정된 값으로 사용하는 불변값(3.14)으로 고정
+    public static final Double circumference = 3.14;
+
+    //거듭제곱
+    public static final Integer POW = 2; //모든 영역에서 고정된 값으로 사용하는 불변값(2)으로 고정
+    public static int getCircle(int r) {
+        return (int) Math.pow(r, POW);
+    }
+
+    public ArrayList<Double> getCircleList(){
+        return circleList;
+    }
+
+    public void setAddCircleList(int r){
+       double circle = calculateCircleArea(r);
+       circleList.add(circle);
+    }
+
 }
